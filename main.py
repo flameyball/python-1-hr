46a0a3a438ae80f07b6bb53806771f1672aebbda
Keir Coons <>
2022-08-26 03:34:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+name = input("what is your name? ")
+print("hi " + name)
+are_online = True
+year = input("how old are you? ")
+age = 2022 - int(year)
+print(age)
+print(are_online)
+
+
+#calculator
+number = input("number 1: ") 
+numb = input("number 2: ")
+output = float(numb) + float(number)
+print ("sum: " + str(output))
+
+number = float(input("number 1: ")) 
+numb = float(input("number 2: "))
+output = number + numb 
+print ("sum: " + str(output))
+
+
+#strings 
+course = 'python for beginners'
+print(course.upper())
+print(course)
