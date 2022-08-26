c7137c68d2c3a8aeffeb11ca617dd3dba5cf3001
Keir Coons <>
2022-08-26 04:22:37 +0000

    added more sections #strings #arithmetic operations #comparison operators

--- main.py
+++ main.py
@@ -23,3 +23,18 @@ print ("sum: " + str(output))
 course = 'python for beginners'
 print(course.upper())
 print(course)
+course = 'python for beginners'
+print(course.replace('for', '4'))
+print('python' in course)
+
+#arithmetic operations
+
+print(10+3, 10-3, 10*3, 30 / 3)
+x=10
+print(x+5, x-5, x*5, x / 5)
+
+#operator precedence
+
+
+#comparison operators
+
